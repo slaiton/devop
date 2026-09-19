2378287a00210562dae93bbf1a61145ecef7db4d
Stiven Laiton <jstivenlaiton@gmail.com>
2026-09-19 12:03:55 -0500

    fix github apps

--- apps/api/src/modules/github-apps/github-apps.module.ts
+++ apps/api/src/modules/github-apps/github-apps.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RolesGuard } from '../../common/roles.guard';
+import { AuthModule } from '../auth/auth.module';
 import { GithubAppsController } from './github-apps.controller';
 import { GithubAppsService } from './github-apps.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [GithubAppsController],
-  providers: [GithubAppsService],
+  providers: [GithubAppsService, RolesGuard],
 })
 export class GithubAppsModule {}
